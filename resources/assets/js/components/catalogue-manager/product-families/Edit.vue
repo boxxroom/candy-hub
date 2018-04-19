38342e26f682389edae1c9c003e5b3e530cfb843
Alec Ritson <alec@neondigital.co.uk>
2018-04-19 17:11:02 +0100

    Product family initial screens

--- resources/assets/js/components/catalogue-manager/product-families/Edit.vue
+++ resources/assets/js/components/catalogue-manager/product-families/Edit.vue
@@ -8,8 +8,10 @@
                 translating: false,
                 languages: [],
                 translationLanguage: locale.current(),
+                fields: {},
+                attribute_groups: [],
                 params: {
-                    includes: 'attributes'
+                    includes: 'attribute_groups.attributes'
                 }
             }
         },
@@ -23,13 +25,12 @@
                 apiRequest.send('get', '/product-families/' + this.id, [], this.params)
                     .then(response => {
                         this.family = response.data;
-                        console.log(response);
-                        // this.fields['name'] = {
-                        //     value: this.group.name,
-                        //     type: 'text',
-                        //     translatable: true
-                        // };
-                        // this.attributes = this.group.attributes.data;
+                        this.fields['name'] = {
+                            value: this.family.name,
+                            type: 'text',
+                            translatable: true
+                        };
+                        this.attribute_groups = this.family.attribute_groups.data;
                         this.loaded = true;
                     });
             },
@@ -51,9 +52,50 @@
 <template>
     <div>
         <template v-if="loaded">
-            <candy-tab name="Product Details" :selected="true" dispatch="product-details">
+            <div class="panel">
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="col-xs-12 text-right">
+                                <div class="form-inline">
+                                    <div class="form-group">
+                                        <div v-show="translating">
+                                            <label class="sr-only">Language</label>
+                                            <candy-select :options="languages" v-model="translationLanguage" v-if="languages.length"></candy-select>
+                                        </div>
+                                    </div>
+                                    <button v-if="!translating" class="btn btn-default" @click="translating = true">Translate</button>
+                                    <button v-if="translating" class="btn btn-default" @click="translating = false">Hide Translation</button>
+                                </div>
 
-            </candy-tab>
+                            </div>
+                            <candy-option-translatable :fields="fields"
+                                :params="{'translating':translating, 'language':translationLanguage}">
+                            </candy-option-translatable>
+
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <h4>Attributes</h4>
+                                </div>
+                                <div class="col-md-4 text-right">
+                                    <button class="btn btn-primary">
+                                        <fa icon="plus"></fa> Add Attribute
+                                    </button>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="panel panel-default" v-for="group in attribute_groups">
+                                <div class="panel-heading"><strong>{{ group.name|t }}</strong></div>
+                                <ul class="list-group">
+                                    <li v-for="attribute in group.attributes.data" class="list-group-item">
+                                        {{ attribute.name|t }}
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </template>
         <div v-else>
             <div class="page-loading loading">
